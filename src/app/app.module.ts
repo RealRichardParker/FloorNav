e544ddea791ddb085e6ef3e040de4d59442ae7e3
Oceanwall <matthew853211@gmail.com>
2018-02-24 17:23:18 -0600

    first lmao

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
