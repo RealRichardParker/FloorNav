5df1cc431dcf8fabc23f61f3b25f2efa45ff34b0
Oceanwall <matthew853211@gmail.com>
2018-02-24 21:00:51 -0600

    fancy ui for upload page, xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { ProcessComponent } from './process/process.component';
     ProcessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,9 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges, AfterViewInit } from '@angular/core';
+import { trigger, state, style, animate, keyframes, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
+  animations: [
+    trigger('bounceIn', [
+      transition('inactive => active', animate(750, keyframes([
+        style({opacity: 0, transform: 'scale3d(0.3, 0.3, 0.3)', offset: 0}),
+        style({transform: 'scale3d(1.1, 1.1, 1.1)', offset: 0.2}),
+        style({transform: 'scale3d(0.9, 0.9, 0.9)', offset: 0.4}),
+        style({opacity: 1, transform: 'scale3d(1.03, 1.03, 1.03)', offset: 0.6}),
+        style({transform: 'scale3d(0.97, 0.97, 0.97)', offset: 0.8}),
+        style({opacity: 1, transform: 'scale3d(1, 1, 1)', offset: 1}),
+      ]))),
+    ])
+  ]
 })
 export class UploadComponent implements OnInit {
 
@@ -12,11 +25,21 @@ export class UploadComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.bounceOnce()
+    }, 1);
+  }
+
+  bounceState : string = "inactive";
+
   fileToUpload: File = null;
 
   fileAdded : boolean = false;
 
   submitted : boolean = false;
+
+  unOpaque : boolean = true;
 
   handleFileInput(files: FileList) {
     this.fileToUpload = files.item(0);
@@ -25,6 +48,15 @@ export class UploadComponent implements OnInit {
 
   prepare() : void {
     this.submitted = true;
+  }
+
+  bounceOnce() : void {
+    this.bounceState = "active";
+  }
+
+  endBounce() : void {
+    this.bounceState = "inactive";
+    this.unOpaque = false;
   }
 
 }
