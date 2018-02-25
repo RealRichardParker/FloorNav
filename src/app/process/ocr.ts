ae54b6280fa6b9e1e5860107de82bee1670641fa
Tiger <tiger.c.zhang@gmail.com>
2018-02-25 05:50:38 -0600

    Can parse locations of all words

--- src/app/process/ocr.ts
+++ src/app/process/ocr.ts
@@ -2,6 +2,7 @@ let himalaya = require('himalaya');
 let fs = require("fs");
 let tesseract = require('tesseract.js');
 let path = require('path');
+let canvas = require('canvas');
 //import * as path from "path";
 //import * as fs from "fs";
 //import * as tesseract from 'tesseract.js';
@@ -12,15 +13,17 @@ let imgDir = "src/assets/first_floor_new.jpg";
 
 let tesseractPromise = tesseract.create({langPath: "eng.traineddata"}).recognize(imgDir, 'eng')
   .progress(message => console.log("current progress: ", message))
-  .then(result => parseTesseractResults(result));
+  .then(result => {parseTesseractResults(result)
+    .then(fulfill => {
+      drawRect()
+        .then((fulfilled) => {
+          console.log("finished covering words: ");
+      });
+  })});
 
-function parseTesseractResults(result) {
+async function parseTesseractResults(result) {
   //console.log(result);
-  //let out = util.inspect(result, {showHidden: false, depth: null});
-  //fs.writeFile('src/app/process/ocr.json', result.text + result.html);
-  //let parser = new DOMParser();
-  //let doc = parser.parseFromString(result.html, "text/html");
-  console.log(result.html);
+  //console.log(result.html);
 
   //himalaya works, cannot access members
   let json = himalaya.parse(result.html);
@@ -28,8 +31,9 @@ function parseTesseractResults(result) {
   promise.then(result => {
     fs.writeFile('output.txt', JSON.stringify(coordsArr), err => {
       console.log(err);
-    })
-  })
+    });
+
+  });
   let object = himalaya.parse(JSON.stringify(json[0]));
   fs.writeFile('src/app/process/ocr.json', JSON.stringify(json), result => {
     console.log(JSON.stringify(object[0]));
@@ -40,6 +44,23 @@ function parseTesseractResults(result) {
 
 }
 let coordsArr = [];
+
+async function drawRect() {
+  //console.log(coordsArr);
+  let img = new Image();
+  img.dir = imgDir;
+  let ctx = canvas.getContext('2d');
+  let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  let data = imageData.data;
+  for(let val in coordsArr)
+  {
+    let leftX = val[0];
+    let leftY = val[1];
+    let rightX = val[2];
+    let rightY = val[3];
+    console.log(typeof leftX);
+  }
+}
 
 async function searchJson(json) {
   for (let key in json) {
